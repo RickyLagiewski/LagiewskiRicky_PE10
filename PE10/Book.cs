eb899d293d655291563629ddebb9f0948cd6ca44
Richard Lagiewski <rickylagiewski@gmail.com>
2018-09-25 20:52:41 -0400

    changes

---
+++ PE10/Book.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE10
+{
+    class Book
+    {
+        private string title;
+        private string author;
+        private int numberOfPages;
+        private string owner;
+        private int numberOfTimesRead;
+        
+
+        // parameterized constructor
+        public Book(string p_title, string p_author, int p_numberOfPages, string p_owner)
+        {
+            title = p_title;
+            author = p_author;
+            numberOfPages = p_numberOfPages;
+            owner = p_owner;
+            numberOfTimesRead = 0;
+        }
+        public string GetOwner()
+        {
+            return owner;
+        }
+        public int NumberOfTimes()
+        {
+            return numberOfTimesRead;
+        }
+        public void SetNumberOfTimes(int value)
+        {
+            if (value >= numberOfTimesRead)
+            {
+                numberOfTimesRead = value;
+            }
+        }
+        public void BookTitle()
+        {
+            Console.WriteLine(title);
+        }
+        public void BookAuthor()
+        {
+            Console.WriteLine(author);
+        }
+        public void BookPages()
+        {
+            Console.WriteLine(numberOfPages);
+        }
+        public void BookOwner()
+        {
+            Console.WriteLine("Who is the new owner?");
+            owner = Console.ReadLine();
+        }
+        public void ReadBook()
+        {
+            numberOfTimesRead++;
+            Console.WriteLine(numberOfTimesRead);
+        }
+        public void PrintBook()
+        {
+            Console.WriteLine("\n Title: " + title);
+            Console.WriteLine("\n Author: " + author);
+            Console.WriteLine("\n Number of Pages: " + numberOfPages);
+            Console.WriteLine("\n Owner: " + owner);
+            Console.WriteLine("\n Number of Times Read: " + numberOfTimesRead);
+        }
+    }
+}
---
+++ PE10/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book myCustomBook = new Book("Memes 101", "Ricky",420, "ME");
+
+            string choice = "";
+
+            while (choice != "done")
+            {
+                Console.WriteLine("Type 'title', 'author', 'pages', 'borrow', 'read', 'print', 'done'\n");
+                choice = Console.ReadLine();
+                Console.WriteLine("");
+
+                if (choice == "title")
+                {
+                    myCustomBook.BookTitle();
+                    Console.WriteLine("");
+                }
+                if (choice == "author")
+                {
+                    myCustomBook.BookAuthor();
+                    Console.WriteLine("");
+                }
+                if (choice == "pages")
+                {
+                    myCustomBook.BookPages();
+                    Console.WriteLine("");
+                }
+                if (choice == "borrow")
+                {
+                    myCustomBook.BookOwner();
+                    Console.WriteLine("");
+                }
+                if (choice == "read")
+                {
+                    myCustomBook.ReadBook();
+                    Console.WriteLine("");
+                }
+                if (choice == "print")
+                {
+                    myCustomBook.PrintBook();
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
